f6c65cbdc6d844011ddab094da0c222a26267b14
Frank Abelbeck <frank.abelbeck@googlemail.com>
2015-06-27 13:22:55 +0200

    initial version

---
+++ OSMTools.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+"""
+OSMTools.py: collection of helper functions for latitude/longitude calculations
+Copyright (C) 2015 Frank Abelbeck <frank.abelbeck@googlemail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import math
+
+
+def lat_to_y(lat,zoom):
+	"""Calculates the y tile coordinate of given latitude at given zoom level.
+
+Args:
+	lat  - latitude given in degrees (float; <0 south of equator)
+	zoom - zoom level (integer, 0..18)
+
+Returns:
+	a float
+
+Raises:
+	ValueError - latitude out of range (-90 < lat < 90)"""
+	return 2**(zoom-1) * (1 - math.log(math.tan(math.radians(lat)) + (1 / math.cos(math.radians(lat)))) / math.pi)
+
+
+def lon_to_x(lon,zoom):
+	"""Calculates the x tile coordinate of given longitude at given zoom level.
+
+Args:
+	lon  - longitude given in degrees (float; <0 west of equator)
+	zoom - zoom level (integer, 0..18)
+
+Returns:
+	a float"""
+	return 2**zoom * (lon + 180) / 360
+
+
+def y_to_lat(y,zoom):
+	"""Calculates the latitude corresponding to given y coordinate and zoom level.
+
+Args:
+	y    - y tile coordinate (float)
+	zoom - zoom level (integer, 0..18)
+
+Returns:
+	a float
+
+Raises:
+	OverflowError - y value out of range."""
+	return math.degrees(math.atan(math.sinh(math.pi - 2*math.pi*y/2**zoom)))
+
+
+def x_to_lon(lon,zoom):
+	"""Calculates the x tile coordinate of given longitude at given zoom level.
+
+Args:
+	lon  - longitude given in degrees (float; <0 west of equator)
+	zoom - zoom level (integer, 0..18)
+
+Returns:
+	a float"""
+	return x*360/2**zoom - 180
+
+
+TROPIC_CANCER    =  (23 + 26/60 + 14.2/3600) # status 2015-06-23, see https://en.wikipedia.org/wiki/Tropic_of_Cancer
+TROPIC_CAPRICORN = -(23 + 26/60 + 14.2/3600) # status 2015-06-24, see https://en.wikipedia.org/wiki/Tropic_of_Capricorn
+ARTIC_CIRCLE     =  (66 + 33/60 + 45.8/3600) # status 2015-06-24, see https://en.wikipedia.org/wiki/Arctic_Circle
+ANTARCTIC_CIRCLE = -(66 + 33/60 + 45.8/3600) # status 2015-06-24, see https://en.wikipedia.org/wiki/Antarctic_Circle
+EQUATOR          = 0
+PRIME_MERIDIAN   = 0
+
+
+def tropic_of_cancer(zoom):
+	"""Calculates the y coordinate of the Tropic of Cancer at given zoom level.
+
+Args:
+	zoom - zoom level (integer, 0..18)
+
+Returns:
+	a float with the y tile coordinate"""
+	return lat_to_y(TROPIC_CANCER,zoom)
+
+
+def tropic_of_capricorn(zoom):
+	"""Calculates the y coordinate of the Tropic of Capricorn at given zoom level.
+
+Args:
+	zoom - zoom level (integer, 0..18)
+
+Returns:
+	a float with the y tile coordinate"""
+	return lat_to_y(TROPIC_CAPRICORN,zoom)
+
+
+def arctic_circle(zoom):
+	"""Calculates the y coordinate of the Artic Circle at given zoom level.
+
+Args:
+	zoom - zoom level (integer, 0..18)
+
+Returns:
+	a float with the y tile coordinate"""
+	return lat_to_y(ARCTIC_CIRCLE,zoom)
+
+
+def antarctic_circle(zoom):
+	"""Calculates the y coordinate of the Antartic Circle at given zoom level.
+
+Args:
+	zoom - zoom level (integer, 0..18)
+
+Returns:
+	a float with the y tile coordinate"""
+	return lat_to_y(ANTARCTIC_CIRCLE,zoom)
+
+
+def equator(zoom):
+	"""Calculates the y coordinate of the Equator at given zoom level.
+
+Args:
+	zoom - zoom level (integer, 0..18)
+
+Returns:
+	a float with the y tile coordinate"""
+	return lat_to_y(EQUATOR,zoom)
+
+
+def prime_meridian(zoom):
+	"""Calculates the x coordinate of the Prime Meridian at given zoom level.
+
+Args:
+	zoom - zoom level (integer, 0..18)
+
+Returns:
+	a float with the x tile coordinate"""
+	return lat_to_y(PRIME_MERIDIAN,zoom)
+
---
+++ createMap.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+"""
+createMap.py: create a large map image from tile images
+Copyright (C) 2015 Frank Abelbeck <frank.abelbeck@googlemail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import sys,os,argparse,time
+import OSMTools
+import urllib.request,urllib.parse
+import PIL.Image
+
+
+if __name__ == "__main__":
+	
+	# obtain basic program path information
+	progpath = os.path.realpath(os.path.abspath(sys.argv[0]))
+	progdir,progname = os.path.split(progpath)
+	
+	cachedefault = os.path.join(progdir,"cache")
+	
+	# setup argument parser and parse commandline arguments
+	parser = argparse.ArgumentParser(
+		description="Create a raster map from OpenStreetMap tiles.",
+		epilog="SOURCE may Instead of a tile server URL, the keywords 'osm' (openstreetmap.de), 'topo' (opentopomap.org) or 'sat' (maquest aerial) are accepted."
+	)
+	parser.add_argument("--source",default="osm",help="URL of a tile server (akin 'http://name.tld/{0}/{1}/{2}.png'); alternatively, bookmark keywords 'osm' (openstreetmap.de; default), 'topo' (opentopomap.org) or 'sat' (maquest aerial) may be used.")
+	parser.add_argument("--cache",default=cachedefault,help="directory of the tile cache; default: "+cachedefault)
+	parser.add_argument("--delay",type=float,help="time in seconds between downloads")
+	parser.add_argument("ZOOM",type=int,help="zoom factor (0..18)")
+	parser.add_argument("WEST",type=float,help="western boundary of the map (longitude in degrees)")
+	parser.add_argument("NORTH",type=float,help="northern boundary of the map (latitude in degrees)")
+	parser.add_argument("EAST",type=float,help="eastern boundary of the map (longitude in degrees)")
+	parser.add_argument("SOUTH",type=float,help="southern boundary of the map (latitude in degrees)")
+	parser.add_argument("FILE",help="name of the output image file")
+	args = parser.parse_args()
+	
+	# make sure cache directory exists
+	if not os.path.exists(args.cache): os.mkdir(args.cache)
+	
+	# check tile server url ("source")
+	if args.source == "osm":
+		source = "http://tile.openstreetmap.de/tiles/osmde/{0}/{1}/{2}.png"
+	elif args.source == "topo":
+		source = "http://opentopomap.org/{0}/{1}/{2}.png"
+	elif args.source == "sat":
+		source = "http://otile1.mqcdn.com/tiles/1.0.0/sat/{0}/{1}/{2}.jpg"
+	else:
+		source = args.source
+		try:
+			source.format(1,2,3)
+		except IndexError:
+			print("Invalid source URL provided!")
+			sys.exit(1)
+	
+	# check bounding box values
+	if args.EAST <= args.WEST or args.NORTH <= args.SOUTH:
+		print("Invalid bounding box values! Required: WEST < EAST and SOUTH < NORTH")
+		sys.exit(1)
+	
+	# check zoom factor
+	zoom = args.ZOOM
+	if zoom < 0 or zoom > 18:
+		print("Invalid zoom factor!")
+		sys.exit(1)
+	
+	# upper left corner of map
+	x0 = int(OSMTools.lon_to_x(args.WEST,zoom))
+	y0 = int(OSMTools.lat_to_y(args.NORTH,zoom))
+	
+	# lower right corner of map
+	x1 = int(OSMTools.lon_to_x(args.EAST,zoom))
+	y1 = int(OSMTools.lat_to_y(args.SOUTH,zoom))
+	
+	tiles = [(zoom,x,y) for x in range(x0,x1) for y in range(y0,y1)]
+	n     = len(tiles)
+	
+	# calculate image dimensions based on the standard 256x256 tile
+	w = (x1 - x0) * 256
+	h = (y1 - y0) * 256
+	
+	# open map image file
+	img = PIL.Image.new("RGB",(w,h))
+	
+	# iterate over tiles
+	for i,tile in enumerate(tiles):
+		
+		# parse tile URL
+		url = source.format(*tile)
+		scheme,hostname,path,params,query,fragment = urllib.parse.urlparse(url)
+		pathname = os.path.join(args.cache,hostname,path[1:])
+		
+		# check if tile is already cached; download it otherwise
+		if os.path.exists(pathname):
+			print("{0}: {1}/{2} cached, skipping".format(url,i,n))
+		else:
+			print("{0}: {1}/{2} downloading...".format(url,i,n),end="")
+			dirname,filename = os.path.split(pathname)
+			os.makedirs(dirname,exist_ok=True)
+			# control download rate
+			try:
+				time.sleep(float(args.delay))
+			except:
+				pass # no valid delay specified: ignore
+			imgbytes = urllib.request.urlopen(url,timeout=5).read()
+			with open(pathname,"wb") as f:
+				f.write(imgbytes)
+			print(" done")
+		
+		# load tile image
+		tileimg = PIL.Image.open(pathname)
+		
+		# paste tile image to map image
+		img.paste(tileimg, (256 * (tile[1] - x0), 256 * (tile[2] - y0)))
+		
+	
+	# save map image file
+	img.save(args.FILE)
